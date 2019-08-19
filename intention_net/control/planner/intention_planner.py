fef3d4bd6b88367342e8d09ef4d0a97f2f5edbaa
lamvanein <oopsxilitol@gmail.com>
2019-08-19 12:21:39 +0800

    3sensors

--- intention_net/control/planner/intention_planner.py
+++ intention_net/control/planner/intention_planner.py
@@ -314,7 +314,8 @@ class IntentionPlanner(object):
 
 		current_angle = 0
 		if self.localizer.last_pose:
-			current_angle = pu.angle_pose_pair(self.localizer.last_pose, path[self.current_idx])
+			#current_angle = pu.angle_pose_pair(self.localizer.last_pose, path[self.current_idx])
+			current_angle = pu.angle_pose_pair(path[self.current_idx],path[self.current_idx+LOCAL_SHIFT])
 		# ignore some beginning position
 		for i in range(int(NUM_INTENTION/5)):
 			self.ahead_idx = get_valid_next_idx(self.ahead_idx)
@@ -328,16 +329,25 @@ class IntentionPlanner(object):
 		self.pub_cur_pose.publish(self.marker_strip(path[self.current_idx : self.current_idx+LOCAL_SHIFT*NUM_INTENTION]))
 		self.pub_last_pose.publish(self.marker_for_last_pose([self.localizer.last_pose,path[self.current_idx]]))
 		
+		turning_angle = reduce(lambda x, y: x + y, intention.data) / len(intention.data)
+
+		'''	
 		print('='*30)
 		print('lst pos:')
 		print(pu.pose(self.localizer.last_pose).position)
 		print('current pos:')
 		print(pu.pose(path[self.current_idx]).position)
 		print('nxt pose:')
-		print(pu.pose(path[self.current_idx+10]).position)
-
-		turning_angle = reduce(lambda x, y: x + y, intention.data) / len(intention.data)
-		temp = [t * 180 / 3.14 for t in intention.data]
+		print(pu.pose(path[self.current_idx+20]).position)
+		print('v1: ')
+		print(pu.angle_pose_pair(self.localizer.last_pose,path[self.current_idx])*180/3.14)
+		print('v2: ')
+		print(pu.angle_pose_pair(path[self.current_idx],path[self.current_idx+10])*180/3.14)
+		print('v3: ')
+		print(pu.angle_pose_pair(path[self.current_idx+10],path[self.current_idx+20])*180/3.14)
+		'''	
+		# temp = [t * 180 / 3.14 for t in intention.data]
+		print('turning angle: %s'%(turning_angle*180/3.14))
 		
 		if turning_angle > TURNING_THRESHOLD:
 			intention = config.LEFT
